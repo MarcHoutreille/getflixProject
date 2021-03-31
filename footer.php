2b3f618d4bd6b222ddcd83580e8bbd156902af48
ChristianJadot <christian.jadot@gmail.com>
2021-03-31 14:07:17 +0200

    upload footer php

---
+++ footer.php
@@ -0,0 +1,12 @@
+        <footer>    
+            <section class="row">
+                <section class="col-12 text-center">
+                    <h6>Follow Us</h6>
+                    <i class="fab fa-facebook fa-lg"></i>
+                    <i class="fab fa-twitter-square fa-lg"></i>
+                    <i class="fab fa-pinterest fa-lg"></i>
+                    <i class="fab fa-instagram-square fa-lg"></i>
+                </section>
+                <section class="col-12 text-center pt-2 copy">Copyright 2021 © GetFlix. All rights reserved.</section>
+            </section>
+        </footer>
