e72d21e9e5fe7e2945d0c7b32d33540775eeff77
MarcHoutreille <marc.houtreille@gmail.com>
2021-04-02 10:49:16 +0200

    how to list all movies ?

--- list_all_movies.php
+++ list_all_movies.php
@@ -1,4 +1,6 @@
 <?php
 include("connexion-db.php");
-
-$req = $db->query('SELECT * FROM movies')
+$req = $db->query('SELECT * FROM movies ORDER BY title ASC');
+while ($movie = $req->fetch()) {
+    echo "<p>" . $movie['title'] . " " .  $movie['duration'] . " minutes</p> ";
+}
