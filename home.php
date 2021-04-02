a4bc46f51950daa8e260b2e656fb59f44aecc55a
MarcHoutreille <marc.houtreille@gmail.com>
2021-04-02 12:34:45 +0200

    Adjust the fucking Bootstrap CDN

--- home.php
+++ home.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="css/getflix-style.css">
     <title>Home</title>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -54,73 +54,70 @@
         <div class="row">
             <h3> Les nouveautés</h3>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
         </div>
         <div class="row">
-        <h3> Les immanquables</h3>
+            <h3> Les immanquables</h3>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-        </div>
-        <div class="row">
-        <h3> Reprendre la lecture</h3>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
         </div>
         <div class="row">
-        <h3> Notre séléction pour vous</h3>
+            <h3> Reprendre la lecture</h3>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
             <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
+                <img src="https://placeimg.com/90/90/tech">
             </div>
         </div>
-        <?php include('phone_navbar.php');?>
+        <div class="row">
+            <h3> Notre séléction pour vous</h3>
+            <div class="col-3">
+                <img src="https://placeimg.com/90/90/tech">
+            </div>
+            <div class="col-3">
+                <img src="https://placeimg.com/90/90/tech">
+            </div>
+            <div class="col-3">
+                <img src="https://placeimg.com/90/90/tech">
+            </div>
+            <div class="col-3">
+                <img src="https://placeimg.com/90/90/tech">
+            </div>
+        </div>
+        <?php include('phone_navbar.php'); ?>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     </script>
 </body>
 
