2514ed1c4afb347df57a27b4fdf6aeeba11d8f60
ChristianJadot <christian.jadot@gmail.com>
2021-04-06 15:36:02 +0200

    maj home php

--- home.php
+++ home.php
@@ -53,84 +53,75 @@
                 </div>
             </div>
         </div>
-        <div class="row">
+        <div class="row my-3">
 
-            <h3> Les nouveautés</h3>
+            <h3>Les nouveautés</h3>
             <?php
-                $response = $db->query('SELECT * FROM movies ORDER BY id DESC LIMIT 4');
+                $response = $db->query("SELECT id, title, year, category, thumbnail, SUBSTRING(synopsis,1,300) AS shortened_synopsis , duration, url, language FROM movies ORDER BY id DESC LIMIT 4");
                 while ($data = $response->fetch()) {
-                    echo '
-                            <div data-id=' . $data['id'] . ' class="card bg-dark text-black movie col-sm-3 px-0 mb-5">
+                    echo '  
+                            <div data-id=' . $data['id'] . ' class="card bg-dark text-black movie col-sm-3 px-0">
                                 <img src=' . $data['thumbnail'] . ' class="card-img" alt=' . $data['title'] . ' >
                                 <div class="card-img-overlay viewTxt">
                                     <h5 class="card-title text-white">' . $data['title'] . '</h5>
-                                    <p class="card-text text-white">' . $data['synopsis'] . '</p>
+                                    <p class="card-text text-white">' . $data['shortened_synopsis'] . '(...)</p>
                                     <p class="card-text text-white">' . $data['duration'] . ' min</p>
-                            </div></div>';
+                                </div></div>';
                 }
             ?>
+        </div>
+        <div class="row my-3">
+                
+            <h3> Les immanquables</h3>
+            <?php
+                $response = $db->query("SELECT id, title, year, category, thumbnail, SUBSTRING(synopsis,1,300) AS shortened_synopsis , duration, url, language FROM movies ORDER BY id DESC LIMIT 4");
+                while ($data = $response->fetch()) {
+                    echo '  
+                            <div data-id=' . $data['id'] . ' class="card bg-dark text-black movie col-sm-3 px-0">
+                                <img src=' . $data['thumbnail'] . ' class="card-img" alt=' . $data['title'] . ' >
+                                <div class="card-img-overlay viewTxt">
+                                    <h5 class="card-title text-white">' . $data['title'] . '</h5>
+                                    <p class="card-text text-white">' . $data['shortened_synopsis'] . '(...)</p>
+                                    <p class="card-text text-white">' . $data['duration'] . ' min</p>
+                                </div></div>';
+                }
+            ?>
+        </div>
+        <div class="row my-3">
+        
+            <h3> Reprendre la lecture</h3>
+            <?php
+                $response = $db->query("SELECT id, title, year, category, thumbnail, SUBSTRING(synopsis,1,300) AS shortened_synopsis , duration, url, language FROM movies ORDER BY id DESC LIMIT 4");
+                while ($data = $response->fetch()) {
+                    echo '  
+                            <div data-id=' . $data['id'] . ' class="card bg-dark text-black movie col-sm-3 px-0">
+                                <img src=' . $data['thumbnail'] . ' class="card-img" alt=' . $data['title'] . ' >
+                                <div class="card-img-overlay viewTxt">
+                                    <h5 class="card-title text-white">' . $data['title'] . '</h5>
+                                    <p class="card-text text-white">' . $data['shortened_synopsis'] . '(...)</p>
+                                    <p class="card-text text-white">' . $data['duration'] . ' min</p>
+                                </div></div>';
+                }
+            ?>
+        </div>
+        <div class="row my-3">
 
-                <!-- 
-            
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
+            <h3> Notre séléction pour vous</h3>
+            <?php
+                $response = $db->query("SELECT id, title, year, category, thumbnail, SUBSTRING(synopsis,1,300) AS shortened_synopsis , duration, url, language FROM movies ORDER BY id DESC LIMIT 4");
+                while ($data = $response->fetch()) {
+                    echo '  
+                            <div data-id=' . $data['id'] . ' class="card bg-dark text-black movie col-sm-3 px-0">
+                                <img src=' . $data['thumbnail'] . ' class="card-img" alt=' . $data['title'] . ' >
+                                <div class="card-img-overlay viewTxt">
+                                    <h5 class="card-title text-white">' . $data['title'] . '</h5>
+                                    <p class="card-text text-white">' . $data['shortened_synopsis'] . '(...)</p>
+                                    <p class="card-text text-white">' . $data['duration'] . ' min</p>
+                                </div></div>';
+                }
+            ?>
         </div>
-        <div class="row">
-        <h3> Les immanquables</h3>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-        </div>
-        <div class="row">
-        <h3> Reprendre la lecture</h3>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-        </div>
-        <div class="row">
-        <h3> Notre séléction pour vous</h3>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-            <div class="col-3">
-            <img src="https://placeimg.com/90/90/tech">
-            </div>
-        </div>
-            -->
+        
         <?php include('phone_navbar.php');?>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
