bad6cfc9b2c94df1febceab0036d4757df8a9b3d
Nathalie Anneessens <77076065+nathalie-anneessens@users.noreply.github.com>
2021-04-09 17:14:07 +0200

    cleaning index remove navbars

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <?php 
 session_start();
 include("connexion_db_perso.php");
+// include("connexion_db.php");
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/05be3ed2ff.js" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/getflix-style.css">
+    <?php include("head.php"); ?>
     <title>GETFLIX</title>
 </head>
 
 <body>
-    <?php include("pc_navbar.php"); ?>
-
     <header class="row">
         <div class="col-md-12 text-center my-5">
             <h1>Discover the unlimited collection of movies with <strong>GETFLIX</strong>... <br>Watch it everywhere!
@@ -39,7 +33,6 @@ include("connexion_db_perso.php");
         </div>
     </main>
 
-    <?php include("phone_navbar.php"); ?>
     <?php include("footer.php"); ?>
 
 </body>
